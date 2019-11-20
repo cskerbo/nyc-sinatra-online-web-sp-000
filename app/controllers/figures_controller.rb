dda7cf3c779b0ca82193e3614938617a6b3fc519
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-20 19:59:32 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,11 @@
 class FiguresController < ApplicationController
+
   get '/figures' do
     @figures = Figure.all
     erb :'figures/index'
+  end
+
+  get '/figure/new' do
+    erb :'/figures/new'
   end
 end
