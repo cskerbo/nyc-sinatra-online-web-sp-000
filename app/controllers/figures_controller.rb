2ce40b4baa4d29b4680f578bf89c48467da3fd14
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-20 20:36:27 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,5 +13,24 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(:name => params[:figure][:name])
+
+    title_entry = params[:figure][:title]
+    if Title.find_by(:name => title_entry)
+      title = Title.find_by(:name => title_entry)
+    else
+      title = Title.create(:name => title_entry)
+    end
+    @figure.title = title
+
+    genre_selections = params[:song][:genres]
+    if genre_selections != nil
+      genre_selections.each do |genre|
+      @song.genres << Genre.find(genre)
+      end
+    end
+
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to "songs/#{@song.slug}"
   end
 end
