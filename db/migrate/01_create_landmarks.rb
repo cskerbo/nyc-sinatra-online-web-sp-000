cb32143e8887d6ca7f636230aaaff5cd00ab36e4
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-20 19:24:39 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,6 +2,8 @@ class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmarks do |l|
       l.string :name
+      l.integer :figure_id
+      l.integer :year_completed
     end
   end
 end
