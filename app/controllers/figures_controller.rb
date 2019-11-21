7ea4f91cd9b39f5e9821a0e214a05e68d2723030
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-21 00:08:46 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,4 +33,17 @@ class FiguresController < ApplicationController
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if params[:title][:name].present?
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark][:name].present?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
 end
